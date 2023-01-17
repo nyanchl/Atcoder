55bbf5a7b7db05b80dcd1956a1ca8b7695a65fa8
nyanchi <rotdicm@gmail.com>
2023-01-17 10:46:51 +0900

    ABC285

---
+++ ABC285.py
@@ -0,0 +1,25 @@
+#A
+# a,b = map(int,input().split())
+# if b == a*2:
+#     print("Yes")
+# elif b == a*2+1:
+#     print("Yes")
+# else:
+#     print("No")
+
+#B
+# N = int(input())
+# S = input()
+# for i in range(1,N):
+#     l = 0
+#     for x, y in zip(S, S[i:]):
+#         if x == y:
+#             break
+#         l += 1
+ 
+#     print(l)
+
+#C
+S = input()
+
+
