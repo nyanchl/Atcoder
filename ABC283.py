eae6fbeb0539b0fac1b0679b2250aeac1040576c
nyanchi <rotdicm@gmail.com>
2023-01-17 10:47:20 +0900

    ABC283

--- ABC283.py
+++ ABC283.py
@@ -1,6 +1,8 @@
 #A
-A,B = map(int,input().split())
-ans = A
-for i in range(1,B):
-  ans *= A
-print(ans)
+# A,B = map(int,input().split())
+# ans = A
+# for i in range(1,B):
+#   ans *= A
+# print(ans)
+
+#B
