ee1051a570d46a09fda6406c8803a640e959e2a6
nyanchi <rotdicm@gmail.com>
2023-01-18 10:15:10 +0900

    ABC283

--- ABC283.py
+++ ABC283.py
@@ -6,3 +6,12 @@
 # print(ans)
 
 #B
+# N = int(input())
+# A = list(map(int,input().split()))
+# Q = int(input())
+# for n in range(Q):
+#   query = list(map(int,input().split()))
+#   if query[0]==2:
+#     print(A[(query[1])-1])
+#   elif query[0]==1:
+#     A[(query[1])-1] = (query[2])
