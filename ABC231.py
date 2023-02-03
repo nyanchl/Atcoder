7201018d64798c1fdc80bb7c48a64020c6b222f0
nyanchi <rotdicm@gmail.com>
2023-02-03 10:45:31 +0900

    ABC231

---
+++ ABC231.py
@@ -0,0 +1,16 @@
+#C
+N,Q = map(int,input().split())
+A = list(map(int,input().split()))
+A = sorted(A)
+for i in range(Q):
+    x = int(input())
+    left = 0
+    right = N
+    while (left != right):
+        mid = (left + right)//2
+        if (A[mid]>x):
+            right = mid
+        else:
+            left = mid+1
+    ans = left
+    print(N-ans)
