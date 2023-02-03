354cb4d2c47e292d4c912a2141c6cb6f1ac4adf0
nyanchi <rotdicm@gmail.com>
2023-02-03 10:46:19 +0900

    ABC279

---
+++ ABC279.py
@@ -0,0 +1,19 @@
+#A
+# S = input()
+# count = 0
+# for l in S:
+#     if l=="w":
+#         count += 2
+#     else:
+#         count += 1
+
+# print(count)
+
+#B
+# S = input()
+# T = input()
+# if S in T:
+#     print("Yes")
+# else:
+#     print("No")
+
