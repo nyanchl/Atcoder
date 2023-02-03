ef9c3ad14a35ce3562813ae3ba18605b570b05ec
nyanchi <rotdicm@gmail.com>
2023-02-03 10:46:54 +0900

    ABC286

--- ABC286.py
+++ ABC286.py
@@ -1,7 +1,18 @@
 #A
-N, P, Q, R, S = map(int, input().split())
-A = list(map(int, input().split()))
-ans = A.copy()
-ans[P - 1:Q] = A[R - 1:S]
-ans[R - 1:S] = A[P - 1:Q]
-print(*ans)
+# N, P, Q, R, S = map(int, input().split())
+# A = list(map(int, input().split()))
+# ans = A.copy()
+# ans[P - 1:Q] = A[R - 1:S]
+# ans[R - 1:S] = A[P - 1:Q]
+# print(*ans)
+
+#B
+N = int(input())
+S = input()
+str = []
+str1 = []
+for i in range(len(S)):
+    print(i)
+    str.append(i)
+    if str=="na":
+        str1 += "nya"
