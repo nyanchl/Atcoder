2703ad1958eec3205e7f78c1a11ae6ebd5c02052
nyanchi <rotdicm@gmail.com>
2023-02-03 10:47:02 +0900

    ABC287

--- ABC287.py
+++ ABC287.py
@@ -25,32 +25,43 @@
 # for i in s:
 #     print(i[:-1])
 #     for n in range(T):
-#         if i==n:
+#         if i[3:]==str(n):
 #             count += 1
 
+# N, M = map(int, input().split())
+# S = [input() for _ in range(N)]
+# T = [input() for _ in range(M)]
+
+# cnt = 0
+# for s in S:
+#     if s[3:] in T:
+#         cnt += 1
+# print(cnt)
+
+
 #C
-def dfs(p,list,seen):
-    seen[p] = True
-    for v2 in list[p]:
-        if seen[v2]: continue
-        dfs(v2, list, seen)
-    return
+# def dfs(p,list,seen):
+#     seen[p] = True
+#     for v2 in list[p]:
+#         if seen[v2]: continue
+#         dfs(v2, list, seen)
+#     return
 
-N,M = map(int,input().split())
+# N,M = map(int,input().split())
 
-list = [[]for _ in range(M)]
+# list = [[]for _ in range(N)]
 
-for i in range(M):
-    u,v = map(int,input().split())
-    list[u].append(v)
-    list[v].append(u)
+# for i in range(M):
+#     u,v = map(int,input().split())
+#     list[u].append(v)
+#     list[v].append(u)
 
-for p in range(N):
-   list[p].sort()
+# for p in range(N):
+#    list[p].sort()
 
-seen = [False for i in range(N)]
-ans = 0
-for p in range(N):
-    if seen[p] == True:
-        ans += 1
-print(ans)
+# seen = [False for i in range(N)]
+# ans = 0
+# for p in range(N):
+#     if seen[p] == True:
+#         ans += 1
+# print(ans)
