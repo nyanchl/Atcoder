fdd1eea46d3091a76bb5bfcaa3cac996d4a5dc77
nyanchi <rotdicm@gmail.com>
2023-02-13 15:24:40 +0900

    ABC289

---
+++ ABC289.py
@@ -0,0 +1,15 @@
+#A
+# s=input()
+# S=""
+# for i in s:
+#   if i=="1":
+#     S+="0"
+#   if i=="0":
+#     S+="1"
+# print(S)
+
+#B
+# N,M = map(int,input().split())
+# for i in range(M):
+#     a = int(input())
+# for i in range(N):
