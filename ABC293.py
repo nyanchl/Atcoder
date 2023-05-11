8906cccefe55863378cde57bc7662b0d25446790
nyanchi <rotdicm@gmail.com>
2023-05-11 11:00:50 +0900

    ABC293 A

---
+++ ABC293.py
@@ -0,0 +1,10 @@
+#A
+# S = list(input())
+# eng = []
+# for i in range(1,(len(S)+1)//2+1):
+#     right = 2*i-1-1
+#     left = 2*i-1
+#     l = S[left]
+#     r = S[right]
+#     eng.append(l+r)
+# print(*eng,sep='')
