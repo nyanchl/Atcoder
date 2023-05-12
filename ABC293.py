860fde6f329886f4784b46bd48e56a13ebee3d5d
nyanchi <rotdicm@gmail.com>
2023-05-12 12:06:55 +0900

    ABC293 B

--- ABC293.py
+++ ABC293.py
@@ -8,3 +8,20 @@
 #     r = S[right]
 #     eng.append(l+r)
 # print(*eng,sep='')
+
+#B
+# N = int(input())
+# call = list(map(int,input().split()))
+# list = [False for _ in range(N)]
+# no = []
+# count = 0
+# for i,num in enumerate(call):
+#     if list[i] == True:
+#         continue
+#     list[num-1] = True
+# for i,n in enumerate(list):
+#     if n==False:
+#         count += 1
+#         no.append(i+1)
+# print(count)
+# print(*no)
