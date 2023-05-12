990bd45ee8153b545f44ff5f52daa4679bf55ada
nyanchi <rotdicm@gmail.com>
2023-05-12 12:07:35 +0900

    test

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-def test_args(**n):
-    print('args: ', n)
-    
-# 関数実行
-test_args(name='BMW', price=10000000, purchased='2024-05-05')
+N = int(input())
+call = list(map(int,input().split()))
+list = [False for _ in range(N)]
+no = []
+count = 0
+for i,num in enumerate(call):
+    if list[i] == True:
+        continue
+    list[num-1] = True
+for i,n in enumerate(list):
+    if n==False:
+        count += 1
+        no.append(i+1)
+print(count)
+print(*no)
--- test2.py
+++ test2.py
@@ -1,2 +1,24 @@
-s = "繝阪ぎ�育ｵ碁ｨ難ｼ�	縺ゅ′縺�"
-print(s.encode("shift-jis").decode("utf-8",errors="ignore"))
+def rec(v ,G ,seen):
+    seen[v] = True
+    for v2 in G[v]:
+        if seen[v2]: continue
+        rec(v2, G, seen)
+    return
+        
+
+N,M = map(int,input().split())
+G = [[] for _ in range(N)]
+for i in range(M):
+    a,b = map(int,input().split())
+    G[a].append(b)
+    G[b].append(a)
+seen = [False for i in range(N)]
+print(G)
+
+rec(0 ,G ,seen)
+
+ans = 'Yes'
+
+for v in range(N):
+    if seen[v] == False: ans = 'No'
+print(ans)
