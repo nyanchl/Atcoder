3beb9e0a4a5d791561a2946888d01d82862446c0
nyanchi <rotdicm@gmail.com>
2023-05-13 23:07:14 +0900

    test

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-N = int(input())
-call = list(map(int,input().split()))
-list = [False for _ in range(N)]
-no = []
-count = 0
-for i,num in enumerate(call):
-    if list[i] == True:
-        continue
-    list[num-1] = True
-for i,n in enumerate(list):
-    if n==False:
-        count += 1
-        no.append(i+1)
-print(count)
-print(*no)
+s = "abcdefg"
+print(s.index("d"))
