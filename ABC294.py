b0c1e4fd0588ffc6fcfbac25245366af2f6eb0f4
nyanchi <rotdicm@gmail.com>
2023-05-13 23:07:40 +0900

    ABC294 B

--- ABC294.py
+++ ABC294.py
@@ -6,3 +6,16 @@
 #     if i%2==0:
 #         guu.append(i)
 # print(*guu)
+
+#B
+H,W = map(int,input().split())
+eng = [chr(ord("A")+i) for i in range(26)]
+for i in range(H):
+    A = map(int,input().split())
+    ans = []
+    for i,num in enumerate(A):
+        if num == 0:
+            ans.append(".")
+        else:
+            ans.append(eng[num-1])
+    print(*ans,sep="")
