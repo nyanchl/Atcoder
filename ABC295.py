53b416a6aac3e4e1c774f5aaf5ba845c71da9b96
nyanchi <rotdicm@gmail.com>
2023-05-13 23:07:47 +0900

    ABC295 B

--- ABC295.py
+++ ABC295.py
@@ -1,11 +1,19 @@
 #A
-N = int(input())
-W = list(input().split())
-eng = ["and","not","that","the","you"]
-ans = "No"
-for i in range(N):
-    if W[i] in eng:
-        ans = "Yes"
-        print(ans)
-        exit()
-print(ans)
+# N = int(input())
+# W = list(input().split())
+# eng = ["and","not","that","the","you"]
+# ans = "No"
+# for i in range(N):
+#     if W[i] in eng:
+#         ans = "Yes"
+#         print(ans)
+#         exit()
+# print(ans)
+
+#B
+R,C = map(int,input().split())
+masu = []
+for i in range(R):
+    gyou = list(input().split())
+    masu.append(gyou)
+print(masu)
