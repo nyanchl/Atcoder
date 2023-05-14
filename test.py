a469b8d983f0351b86fe52ff38648718f96fe6ac
nyanchi <rotdicm@gmail.com>
2023-05-14 10:33:58 +0900

    因縁のFizz Buzz

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-s = "abcdefg"
-print(s.index("d"))
+N = int(input())
+for i in range(1,N+1):
+    if i%3 == 0 and i%5==0:
+        print("Fizz Buzz")
+    elif i%3 == 0:
+        print("Fizz")
+    elif i%5 == 0:
+        print("Buzz")
+    else:
+        print(i)
