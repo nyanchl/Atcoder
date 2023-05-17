30ebf94d25c7288b323dddd0c5f60f33200a4557
nyanchi <rotdicm@gmail.com>
2023-05-17 12:12:27 +0900

    ABC243 B

---
+++ ABC243.py
@@ -0,0 +1,13 @@
+#B
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+count = 0
+point= 0
+for i,str in enumerate(A):
+    if A[i] == B[i]:
+        count += 1
+    if str in B:
+        point += 1
+print(count)
+print(point-count)
