5c02c9104391de626a1464bd7505f81b95604f81
nyanchi <rotdicm@gmail.com>
2023-05-17 12:16:45 +0900

    ABC301 B

---
+++ ABC301.py
@@ -0,0 +1,43 @@
+#A
+# N = int(input())
+# S = input()
+# T = 0
+# A = 0
+# for str in S:
+#     if str=='T':
+#         T += 1
+#     else:
+#         A += 1
+# if T>A:
+#     print("T")
+# elif (T==A) and S[-1:]=="A":
+#     print("T")
+#     exit()
+# else:
+#     print("A")
+
+#B
+N = int(input())
+A = list(map(int,input().split()))
+B = []
+
+for i in range(len(A)-1):
+  if A[i]>A[i+1]:
+    for j in range(A[i]-A[i+1]):
+      B.append(A[i]-j)
+  elif A[i]<A[i+1]:
+    for j in range(A[i+1]-A[i]):
+      B.append(A[i]+j)
+      
+B.append(A[N-1])
+
+for i in range(len(B)):
+  print(B[i],end=" ")
+    
+        
+# for _ in s:
+#     print(_)
+# print(A[i])
+# print(s)
+# if A[i]==_:
+#     print("hoge")
