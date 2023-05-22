393dc46d9f0f4f80de0339a8d96068d0cd5e4d53
nyanchi <rotdicm@gmail.com>
2023-05-22 09:22:49 +0900

    ABC302 A

---
+++ ABC302.py
@@ -0,0 +1,11 @@
+#A
+# A,B = map(int,input().split())
+# if A%B == 0:
+#     print(A//B)
+# else:
+#     print(A//B+1)
+
+#B
+H,W = map(int,input().split())
+mas = [[input()] for _ in range(H)]
+print(mas)
