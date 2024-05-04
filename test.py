42bd3b5a839b91c515ed3baa9439a60372262ec1
Nyanchl <rotdicm@gmail.com>
2024-05-04 13:19:17 +0900

    ABC351 A

--- test.py
+++ test.py
@@ -1,31 +1,7 @@
-H,W = map(int,input().split())
-map = [[] for i in range(H)]
-count = 0
-#横排除
-for i in range(H):
-    a = list(input())
-    if '#' in a:
-        for s in a:
-            map[i-count].append(s)
-    else:
-        map.pop(i-count)
-        count += 1
-#縦排除
-if len(map) == 1:
-    ans = []
-    for i in map[0]:
-        if i != '.':
-            ans.append(i)
-    print(*ans,sep='')
-else:
-    for i,moji in enumerate(map[0]):
-        if moji == '.':
-            how = []
-            for s in map:
-                how.append(s[i])
-            if '#' not in how:
-                for n in map:
-                    n.pop(i)
-                
-    for i in map:
-        print(*i,sep='')
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A_sum = sum(A)
+B_sum = sum(B)
+ans = A_sum - B_sum + 1
+print(ans)
